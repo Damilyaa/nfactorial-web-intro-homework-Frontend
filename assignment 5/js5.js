02976d5bc6901c1f0d3851adf0353e48cb4387b1
Damilya <153479150+Damilyaa@users.noreply.github.com>
2024-10-18 09:40:13 +0500

    Add files via upload

---
+++ assignment 5/js5.js
@@ -0,0 +1,12 @@
+let table = document.getElementById('age-table');
+
+let labels = table.querySelectorAll('label');
+
+let firstTd = table.rows[0].cells[0];
+
+let form = document.querySelector('form[name="search"]');
+
+let firstInput = form.querySelector('input');
+
+let inputs = form.querySelectorAll('input');
+let lastInput = inputs[inputs.length - 1];
