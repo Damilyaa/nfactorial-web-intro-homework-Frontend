c8727455597151c6494d820afbdc2b84fc397450
Damilya <153479150+Damilyaa@users.noreply.github.com>
2024-12-03 15:42:52 +0500

    Add files via upload

---
+++ assignment8/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <Header />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+export default App;
